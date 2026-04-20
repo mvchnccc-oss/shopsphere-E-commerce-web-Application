d31e49078e08ace1b81e00d2bc699b250648e0f9
malfydev <malfydev@gmail.com>
2026-04-20 21:54:07 +0200

    Hide cart, wishlist, and profile from navbar if not logged in

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,9 +10,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { HeartIcon, ShoppingBagIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Navbar() {
+  const { status } = useSession();
+
   return (
     <div className="bg-accent p-3 flex items-center gap-4">
       <Link
@@ -24,34 +28,48 @@ export default function Navbar() {
       <div className="pt-2 flex-1">
         <Link href="/">Home</Link>
       </div>
-      <div className="flex items-center">
-        <Button variant="ghost" size="icon">
-          <HeartIcon />
-        </Button>
-        <Button variant="ghost" size="icon">
-          <ShoppingCartIcon />
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="rounded-full ml-1">
-              <Avatar>
-                <AvatarImage src="" alt="shadcn" />
-                <AvatarFallback>
-                  <UserIcon />
-                </AvatarFallback>
-              </Avatar>
+      <div className="flex items-center pt-1">
+        {status === "unauthenticated" && (
+          <>
+            <Link href="/auth/login" className="mr-1">
+              <Button variant="default">Login</Button>
+            </Link>
+            <Link href="/auth/register">
+              <Button variant="default">Register</Button>
+            </Link>
+          </>
+        )}
+        {status === "authenticated" && (
+          <>
+            <Button variant="ghost" size="icon">
+              <HeartIcon />
             </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-32">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            <Button variant="ghost" size="icon">
+              <ShoppingCartIcon />
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="rounded-full ml-1">
+                  <Avatar>
+                    <AvatarImage src="" alt="shadcn" />
+                    <AvatarFallback>
+                      <UserIcon />
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-32">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        )}
       </div>
     </div>
   );
