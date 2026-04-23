c2af5caaf63e47b2984af59c689e5c41ab5d1a36
malfydev <malfydev@gmail.com>
2026-04-23 19:53:20 +0200

    Add cart link to cart dropdown and extract menu item into its component

---
+++ Frontend/src/components/cart/cart-item.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { CartProduct } from "@/lib/interfaces/cart.interface";
+import { ImageIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import QuantityField from "./quantity-field";
+
+interface CardItemProps {
+  product: CartProduct & { id: string };
+  onClick?: () => void;
+  dark?: boolean;
+}
+
+export default function CartItem({ product, onClick, dark }: CardItemProps) {
+  const [imageError, setImageError] = useState(false);
+
+  return (
+    <Link key={product.id} href={`/products/${product.id}`} onClick={onClick}>
+      <div className="flex items-center gap-2w-full">
+        <div>
+          {imageError || !product.image ? (
+            <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
+              <ImageIcon className="size-16 text-gray-300" />
+              <span className="text-xs text-gray-500">No Image</span>
+            </div>
+          ) : (
+            <Image
+              src={product.image}
+              width={64}
+              height={64}
+              alt={product.title}
+              onError={() => setImageError(true)}
+              className="object-contain w-full h-full transition-transform duration-300 group-hover:scale-105"
+            />
+          )}
+        </div>
+        <div className="flex flex-col gap-2 flex-1 p-2">
+          <span className="text-sm">{product.title}</span>
+          <div className="flex justify-between items-center text-sm text-muted-foreground">
+            <span>
+              Unit <strong>EGP {product.price}</strong>
+            </span>
+            <span>
+              Total <strong>EGP {product.quantity * product.price}</strong>
+            </span>
+          </div>
+          <QuantityField id={product.id} dark={dark} />
+        </div>
+      </div>
+    </Link>
+  );
+}
--- Frontend/src/components/cart/navbar-dropdown.tsx
+++ Frontend/src/components/cart/navbar-dropdown.tsx
@@ -7,17 +7,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { CreditCardIcon, ImageIcon, ShoppingCartIcon } from "lucide-react";
-import Image from "next/image";
+import { CreditCardIcon, ShoppingCart, ShoppingCartIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import CartItem from "./cart-item";
 import { useCart } from "./context";
-import QuantityField from "./quantity-field";
 
 export default function CartNavbarDropdown() {
   const [isOpen, setOpen] = useState(false);
   const { cartProducts } = useCart();
-  const [imageError, setImageError] = useState<string[]>([]);
 
   const count = Object.keys(cartProducts).length;
 
@@ -39,46 +37,29 @@ export default function CartNavbarDropdown() {
           {count !== 0 && (
             <>
               <DropdownMenuItem>
-                <Link href="/checkout" className=" w-full">
+                <Link href="/checkout" className="w-full">
                   <Button className="flex justify-center items-center w-full gap-2">
                     <CreditCardIcon /> Checkout
                   </Button>
                 </Link>
               </DropdownMenuItem>
-              {Object.entries(cartProducts).map(([id, product]) => (
-                <Link key={id} href={`/products/${id}`} onClick={() => setOpen(false)}>
-                  <div className="flex items-center gap-2w-full">
-                    <div>
-                      {imageError.includes(id) || !product.image ? (
-                        <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
-                          <ImageIcon className="size-16 text-gray-300" />
-                          <span className="text-xs text-gray-500">No Image</span>
-                        </div>
-                      ) : (
-                        <Image
-                          src={product.image}
-                          width={64}
-                          height={64}
-                          alt={product.title}
-                          onError={() => setImageError((v) => [...v, id])}
-                          className="object-contain w-full h-full transition-transform duration-300 group-hover:scale-105"
-                        />
-                      )}
-                    </div>
-                    <div className="flex flex-col gap-2 flex-1 p-2">
-                      <span className="text-sm">{product.title}</span>
-                      <div className="flex justify-between items-center text-sm text-muted-foreground">
-                        <span>
-                          Unit <strong>EGP {product.price}</strong>
-                        </span>
-                        <span>
-                          Total <strong>EGP {product.quantity * product.price}</strong>
-                        </span>
-                      </div>
-                      <QuantityField id={id} dark />
-                    </div>
-                  </div>
+              <DropdownMenuItem>
+                <Link href="/cart" className="w-full">
+                  <Button
+                    variant="secondary"
+                    className="flex justify-center items-center w-full gap-2"
+                  >
+                    <ShoppingCart /> Cart
+                  </Button>
                 </Link>
+              </DropdownMenuItem>
+              {Object.entries(cartProducts).map(([id, product]) => (
+                <CartItem
+                  key={id}
+                  product={{ ...product, id }}
+                  onClick={() => setOpen(false)}
+                  dark
+                />
               ))}
             </>
           )}
