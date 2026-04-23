2fd5eec12f25cf297f2c5cde44ab636adfb25d62
malfydev <malfydev@gmail.com>
2026-04-23 02:51:39 +0200

    Fix CartNavbarDropdown bugs
    - Fix QuantityFields buttons by preventing event baubling to Link
    - Disallow any values under 1 for quantity

--- Frontend/src/components/cart/quantity-field.tsx
+++ Frontend/src/components/cart/quantity-field.tsx
@@ -15,6 +15,8 @@ export default function QuantityField({ id, dark }: { id: string; dark?: boolean
   }
 
   function updateQuanitity(value: number) {
+    if (value < 1) return;
+
     const product = cartProducts[id];
     updateCart((cart) => ({ ...cart, [id]: { ...product, quantity: value } }));
   }
@@ -33,24 +35,36 @@ export default function QuantityField({ id, dark }: { id: string; dark?: boolean
         <Trash2Icon />
       </Button>
       <div className="flex">
-        <Button
-          className="bg-black"
-          size="icon-sm"
+        <div
+          className="cursor-default"
           onClick={(e) => {
             e.preventDefault();
             e.stopPropagation();
-            updateQuanitity(cartProducts[id].quantity - 1);
           }}
-          disabled={cartProducts[id].quantity === 1}
         >
-          <MinusIcon />
-        </Button>
+          <Button
+            className="bg-black"
+            size="icon-sm"
+            onClick={() => updateQuanitity(cartProducts[id].quantity - 1)}
+            disabled={cartProducts[id].quantity <= 1}
+          >
+            <MinusIcon />
+          </Button>
+        </div>
         <input
           className={` ${dark ? "text-white" : "text-black"} w-5 text-center outline-0 border-0 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
           type="number"
           min={1}
           value={cartProducts[id].quantity}
           onChange={(e) => updateQuanitity(Number(e.target.value) || 0)}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+          onFocus={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+          }}
         />
         <Button
           className="bg-black"
