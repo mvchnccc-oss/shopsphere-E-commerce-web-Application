e3a537f1cf30d904ff40dd8681c958fe5c57ce41
Mohamed Ahmed <mvchnccc@gmail.com>
2026-04-24 02:56:15 +0300

    ----

--- Frontend/src/app/(pages)/checkout/page.tsx
+++ Frontend/src/app/(pages)/checkout/page.tsx
@@ -4,7 +4,6 @@ import { Truck, ChevronLeft, CheckCircle2 } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useCart } from '@/components/cart/context';
-// --- إضافات اللوجيك ---
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
