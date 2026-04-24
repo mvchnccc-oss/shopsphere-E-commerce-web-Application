5856b6754d8d4042e0247a42afc045c4c5a1ba9f
malfydev <malfydev@gmail.com>
2026-04-24 22:07:39 +0300

    Use LoadingImage in cart dropdown

--- Frontend/src/components/cart/cart-item.tsx
+++ Frontend/src/components/cart/cart-item.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { CartProduct } from "@/lib/interfaces/cart.interface";
-import { ImageIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import LoadingImage from "../loading-image";
 import QuantityField from "./quantity-field";
 
 interface CardItemProps {
@@ -13,27 +11,17 @@ interface CardItemProps {
 }
 
 export default function CartItem({ product, onClick, dark }: CardItemProps) {
-  const [imageError, setImageError] = useState(false);
-
   return (
     <Link key={product.id} href={`/products/${product.id}`} onClick={onClick}>
-      <div className="flex items-center gap-2w-full">
-        <div>
-          {imageError || !product.image ? (
-            <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
-              <ImageIcon className="size-16 text-gray-300" />
-              <span className="text-xs text-gray-500">No Image</span>
-            </div>
-          ) : (
-            <Image
-              src={product.image}
-              width={64}
-              height={64}
-              alt={product.title}
-              onError={() => setImageError(true)}
-              className="object-contain w-full h-full transition-transform duration-300 group-hover:scale-105"
-            />
-          )}
+      <div className="flex items-center gap-2">
+        <div className="max-w-16 max-h-16">
+          <LoadingImage
+            src={product.image}
+            width={64}
+            height={64}
+            alt={product.title}
+            className="object-contain w-full transition-transform duration-300 group-hover:scale-105"
+          />
         </div>
         <div className="flex flex-col gap-2 flex-1 p-2">
           <span className="text-sm line-clamp-1">{product.title}</span>
