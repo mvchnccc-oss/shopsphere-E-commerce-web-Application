f61b2bad21b0f8cfe1ce309e3e81edbe7caa2a94
malfydev <malfydev@gmail.com>
2026-04-29 02:02:41 +0300

    Fix loading image

--- Frontend/src/components/loading-image.tsx
+++ Frontend/src/components/loading-image.tsx
@@ -2,7 +2,7 @@
 import { cn } from "@/lib/utils";
 import { ImageIcon } from "lucide-react";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Spinner } from "./ui/spinner";
 
 interface LoadingImageProps {
@@ -13,15 +13,11 @@ interface LoadingImageProps {
   className?: string;
 }
 
-export default function LoadingImage(props: LoadingImageProps) {
+export default function LoadingImage({ src, width, height, alt, className }: LoadingImageProps) {
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState(false);
-  const imageRef = useRef<HTMLImageElement | null>(null);
 
-  const width = imageRef.current?.width ?? props.width;
-  const height = imageRef.current?.height ?? props.height;
-
-  if (isError || !props.src || props.src.trim() === "")
+  if (isError || !src?.trim())
     return (
       <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
         <ImageIcon className="text-gray-400" />
@@ -30,25 +26,23 @@ export default function LoadingImage(props: LoadingImageProps) {
     );
 
   return (
-    <div
-      className="relative"
-      style={{
-        width,
-        height,
-      }}
-    >
+    <div className="relative" style={{ width, height }}>
       <Image
-        src={props.src}
-        width={props.width}
-        height={props.height}
-        alt={props.alt}
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+        placeholder="empty"
         onLoad={() => setLoading(false)}
         onError={() => setError(true)}
-        className={cn(props.className, isLoading ? "hidden" : "block")}
-        ref={imageRef}
+        className={cn(
+          className,
+          "transition-opacity duration-300",
+          isLoading ? "opacity-0" : "opacity-100",
+        )}
       />
       {isLoading && (
-        <div className="absolute inset-0 flex items-center justify-center z-10">
+        <div className="absolute inset-0 flex items-center justify-center">
           <Spinner className="text-black w-1/4 h-1/4" />
         </div>
       )}
