fecde3b43a43291bcff00ab4eddaebd24c8b9a72
Malfy <malfydev@gmail.com>
2026-05-14 21:43:56 +0300

    fix: security issues
    - Move jwt.secret into .env
    - Fix wrong port number in cors for localhost, and introduce frontend.url for nextjs deployment url in produciton

--- ecommerce-backend/src/main/java/com/abdullah/eCommerce/config/CorsConfig.java
+++ ecommerce-backend/src/main/java/com/abdullah/eCommerce/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.abdullah.eCommerce.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,11 +12,14 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url:}")
+    String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", frontendUrl));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
